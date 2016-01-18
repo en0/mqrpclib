2e15dece767d36fa3e593567bfaaef567fad7963
Ian Laird <irlaird@gmail.com>
2016-01-17 23:31:42 -0700

    Set patch version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="mqrpclib",
-    version="0.2.0",
+    version="0.2.1",
     packages=["mqrpclib"],
     install_requires=['pika'],
     description="Remote Procedure Call using RabbitMQ",
