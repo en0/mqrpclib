a6021bad435a0fc477a22c91e8776c060f0ca8a5
Ian Laird <irlaird@velma.com>
2016-09-03 00:39:57 -0600

    Changed proxy model to take advantage of service name and reused channels.

--- mqrpclib/rpc_proxy_factory.py
+++ mqrpclib/rpc_proxy_factory.py
@@ -1,17 +1,21 @@
+from rpc_proxy import RpcProxy
 from rpc_exception import get_return_or_raise_exception as _get_ex
+from contextlib import contextmanager
+import pika
 
-
-class RpcAutoProxy(object):
-    pass
 
 class RpcProxyFactory(object):
+    """ RPC Proxy Factory can generate RpcProxy objects with service methods.
+    
+    RPC Proxy Factory can generate RpcProxy objects that abstract the
+    interface for the specified remote service.  
+    """
 
-    @classmethod
-    def create(cls, service_proxy, version):
+    def __call__(self, service_name, version, **kwargs):
         """ Create a proxy object that represents a service.
 
         Create a new Type that contains methods as defined by the service
-        connected to the service_proxy.  
+        identified by service_name.
 
         Arguments:
             service_proxy:
@@ -20,74 +24,131 @@ class RpcProxyFactory(object):
             version:
                 Create interface for the version provided
 
-        Returns:
-            A type that, once instantiated, will expose the service methods.
-        """
-        return _create_type(service_proxy, version, False, None);
-
-
-    @classmethod
-    def create_ex(cls, service_proxy, version, extended_codes=None):
-        """ Create a proxy object that represents a service.
-
-        Create a new Type that contains methods as defined by the service
-        connected to the service_proxy.  When a method is invoked, the method
-        will retrieve values useing the get_return_or_raise_exception method.
-
-        Arguments:
-            service_proxy:
-                A RpcProxy that is connected to a service that implements the 
-                _inspect interface.
-            version:
-                Create interface for the version provided
+        Keyword Arguments:
+            timeout: 
+                Override the timeout used to call service._inspect
+            with_exceptions:
+                Implement the proxy methods using get_return_or_raise_exception
+                method.
             extended_codes:
-                Add service specific exception types. For more information, see
-                get_return_or_raise_exception documentation.
+                Included a map to decode service specific error codes to a
+                exception type. Thie argument only applies when with_exceptions
+                is set.
 
         Returns:
             A type that, once instantiated, will expose the service methods.
         """
-        return _create_type(service_proxy, version, True, extended_codes);
 
+        kwargs.setdefault("timeout", self._timeout)
+        kwargs.setdefault("with_exceptions", False)
+        kwargs.setdefault("extended_codes", None)
 
-def _create_type(proxy, version, with_exceptions, extended_codes):
-
-    specs = _get_ex(proxy.remote_exec("_inspect", "built-in"))
-
-    attr = {}
-
-    for spec in specs["methods"]:
-
-        if version != spec["version"]:
-            continue
-
-        attr[spec["method"]] = _create_method(
-            proxy,
-            spec["method"],
-            spec["version"],
-            spec["desc"],
-            with_exceptions,
-            extended_codes
+        service_proxy = RpcProxy(
+            service_name,
+            self._chan,
+            kwargs["timeout"]
         )
 
-    type_name = "".join([specs.get("service", "Auto"), "Proxy"])
+        return self._create_type(
+            service_proxy,
+            version,
+            kwargs["with_exceptions"],
+            kwargs["extended_codes"],
+            kwargs["timeout"]
+        );
 
-    return type(str(type_name), (RpcAutoProxy,), attr)
+    @classmethod
+    @contextmanager
+    def context(cls, url, timeout=None):
+        """ Contextually managed instance of a RpcProxyFactory.
+
+        This context manager will create a pika channel from the provided URL
+        and instanciate a RpcProxyFacotry class for use. Once the context is
+        expired, the channel will be closed.
+
+        Arguments:
+            url:
+                A pika.URLParameters url that defines how to connect to rabbit.
+            timeout:
+                Optional. Define the timout to pass to the initialization of the
+                RpcProxy instance.
+        """
+        conn = pika.BlockingConnection(pika.URLParameters(url))
+        chan = conn.channel()
+        ret = cls(chan, timeout)
+
+        try:
+            yield ret
+        finally:
+            conn.close()
+
+    def __init__(self, chan, timeout=None):
+        """ Initialize an instance of a RpcProxyFactory.
+
+        Arguments:
+            chan:
+                A connected pika channel that has appropriate access to declare
+                queues and consume requests.
+
+            timeout:
+                Optional, argument to pass RpcProxy for calls to
+                service._inspect.
+        """
+        self._chan = chan
+        self._timeout = None
+
+    def _create_type(self, proxy, version, with_ex, codes, timeout):
+
+        specs = _get_ex(proxy.remote_exec("_inspect", "built-in"))
+
+        attr = {
+            "__init__": self._make_init(proxy.service_name, self._chan),
+            "__doc__": specs["description"]
+        }
+
+        for spec in specs["methods"]:
+
+            if version != spec["version"]:
+                continue
+
+            attr[spec["method"]] = self._create_method(
+                spec["method"],
+                spec["version"],
+                spec["desc"],
+                with_ex,
+                codes
+            )
+
+        type_name = "".join([proxy.service_name, "Proxy"])
+
+        return type(str(type_name), (RpcAutoProxy,), attr)
+
+    def _create_method(self, method, version, desc, with_ex, codes):
+
+        def _wrapper(self, *args, **kwargs):
+
+            _ret = self.remote_exec(method, version, args=args, kwargs=kwargs)
+
+            if(with_ex):
+                return _get_ex(_ret, codes)
+
+            return _ret
+
+        _wrapper.__doc__ = desc
+        _wrapper.__name__ = str(method)
+        _wrapper.__service_version__ = str(version)
+
+        return _wrapper
+
+    def _make_init(self, service_name, channel):
+
+        def _wrapper(self):
+            super(RpcAutoProxy, self).__init__(service_name, channel)
+
+        _wrapper.__doc__ = "Create a proxy for the {} service.".format(service_name)
+        _wrapper.__name__ = "__init__"
+        return _wrapper
 
 
-def _create_method(proxy, method, version, desc, with_exceptions, extended_codes):
-
-    def _wrapper(self, *args, **kwargs):
-
-        _ret = proxy.remote_exec(method, version, args=args, kwargs=kwargs)
-
-        if(with_exceptions):
-            return _get_ex(_ret, extended_codes)
-
-        return _ret
-
-    _wrapper.__doc__ = desc
-    _wrapper.__name__ = str(method)
-    _wrapper.__service_version__ = str(version)
-
-    return _wrapper;
+class RpcAutoProxy(RpcProxy):
+    pass
